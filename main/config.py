d84b4a751f8d48063e3e1ad391f4ab83e6651c05
Nived R <11018762@STUD.HOCHSCHULE-HEIDELBERG.DE>
2022-12-20 16:52:14 +0100

    Config file changes

--- main/config.py
+++ main/config.py
@@ -7,8 +7,8 @@ class AppConfig():
         SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI") 
     else:
         #POSTGRESQL URI
-        #SQLALCHEMY_DATABASE_URI = "postgresql://postgres:v4ri4nt@localhost/srhsdp"
-        SQLALCHEMY_DATABASE_URI = "postgresql://nivedsdp:AGNUSWgGoZ74UpmPu3OF7njqsjVwBj6U@dpg-ce365msgqg43k3jg08jg-a.frankfurt-postgres.render.com/sdp"
+        SQLALCHEMY_DATABASE_URI = "postgresql://postgres:v4ri4nt@localhost/srhsdp"
+        #SQLALCHEMY_DATABASE_URI = "postgresql://nivedsdp:AGNUSWgGoZ74UpmPu3OF7njqsjVwBj6U@dpg-ce365msgqg43k3jg08jg-a.frankfurt-postgres.render.com/sdp"
         
     #SESSION SECRET KEY
     SECRET_KEY = b'_5#y2L"F4Q8z\n\xec]/'
